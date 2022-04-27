8db90e1e32cc95590d6694e0e3e54618fef78fc6
Raj Shah <rnshah9@asu.edu>
2022-04-26 19:20:23 -0700

    Add afl to gitignore

--- fuzzing/get-afl.sh
+++ fuzzing/get-afl.sh
@@ -4,11 +4,12 @@ rm afl-latest.tgz 2> /dev/null
 wget http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz || exit
 tar -xzvf afl-latest.tgz
 rm afl-latest.tgz
+
 cd afl-*
 make
 cd llvm_mode
-# may need to prepend LLVM_CONFIG=/usr/bin/llvm-config-3.8 or similar, depending on the system
 LLVM_CONFIG=/usr/bin/llvm-config-10 make 
+
 cd ../..
 rm -rf afl
 mv afl-* afl
